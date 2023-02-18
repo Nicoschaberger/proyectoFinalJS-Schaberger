3e7c148cc61ceeca5c71015e6ef7f4953eebf97c
Nicolas Schaberger <nicolasschaberger.13@hotmail.com>
2023-02-18 00:46:41 -0300

    modificaciones

--- js/main.js
+++ js/main.js
@@ -1,32 +1,96 @@
-const contenedor = document.querySelector("#contenedor");
+const menuContenedor = document.querySelector("#menu-contenedor");
+const btnProductos = document.querySelector("#btnProductos");
+const carroContenedor = document.querySelector("#carro-contenedor")
+const cantidadCarrito = document.querySelector("#cantidadCarrito");
+
 
 let carrito =[];
-
 function init(){
     titulo()
     mostrarMenu()
-    botonera();
-    agregarAlCarrito();
-    recuperar();
+    botonera()
+    agregarAlCarrito()
+    mostrarCarrito()
+    recuperar()
 }
 
 function titulo() {
     const titulo = document.createElement("h1");
     titulo.setAttribute("class", "title");
-    titulo.innerHTML = "Verduleria Java";
-    document.body.appendChild(titulo)
-}
+    titulo.innerHTML = "kiosco Java";
+    menuContenedor.appendChild(titulo)
+  }
+  
+  function mostrarMenu() {
+      productos.forEach((producto) => {
+          const btn = document.createElement("div");
+          btn.setAttribute ("class", "botonProductos");
+          btn.setAttribute("id", `${producto.id}`);
+          btn.innerHTML = ` 
+          <img src="${producto.img}">
+          <h2>${producto.nombre}</h2>
+          <p class="price">${producto.precio} $</p>
+          `;  
+          btnProductos.appendChild(btn);
+      });
+  }
 
-function mostrarMenu() {
-    productos.forEach((producto) => {
-        const btn = document.createElement("button");
-        btn.setAttribute ("class", "botonProductos");
-        btn.setAttribute("id", `${producto.id}`);
-        btn.innerHTML = producto.nombre;
+  function botonera(){
+    let botones = document.getElementsByClassName("botonProductos");
 
-        document.body.appendChild(btn);
-    });
-}
+    for (btn of botones) {
+      btn.addEventListener("click", () =>{
+        let prod = productos.filter ((producto) => producto.id === btn.id)
+      });
+    }
+  }
+    
+  function agregarAlCarrito(){
+    let btnadd = document.getElementsByClassName("botonProductos");
+      for (const botones of btnadd) {
+        botones.addEventListener('click',()=>{
+          let producto = producto.find((item) => item.id == botones.id);
+            carrito.push(producto);
+            mostrarCarrito(producto);
+            totalCarrito()
+            localStorage.setItem('carrito', JSON.stringify(carrito))
+        });          
+      }
+  }    
+  
+  function totalCarrito(){
+      const total = document.getElementById('total')
+      total.innerText = carrito.reduce((acc,el)=> acc + el.precio,0)
+  }
+  
+  function mostrarCarrito(){
+      const carrito = document.getElementById('carrito')
+    let li = document.createElement('li')
+    li.innerHTML += `${producto.nombre} ${producto.precio}`
+    carrito.appendChild(li)
+  }
+  
+  function recuperar() {
+      let recuperoLS = JSON.parse(localStorage.getItem('carrito'))
+      if(recuperoLS){
+        recuperoLS.forEach(item=>{
+          mostrarCarrito(item)
+          carrito.push(item)
+          totalCarrito()
+        })
+      }
+    }
+    
+    const saveLocal = () => {
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+    };
+
+init();
+
+
+
+
+/*
 
 function botonera() {
     let div = document.createElement("div");
@@ -44,46 +108,10 @@ function botonera() {
       }
 }
 
-function agregarAlCarrito(){
-    let btnadd = document.getElementsByClassName("botonProductos");
-        for (const botones of btnadd) {
-          botones.addEventListener('click',()=>{
-          let producto = productos.find((item) => item.id == botones.id);
-          carrito.push(producto);
-            mostrarCarrito(producto);
-            totalCarrito()
-            localStorage.setItem('carrito', JSON.stringify(carrito))
-          })          
-        }
-}
 
-function totalCarrito(){
-    const total = document.getElementById('total')
-    total.innerText = carrito.reduce((acc,el)=> acc + el.precio,0)
-}
-
-function mostrarCarrito(producto){
-    const carrito = document.getElementById('carrito')
-  let li = document.createElement('li')
-  li.innerHTML += `${producto.nombre} ${producto.precio}`
-  carrito.appendChild(li)
-}
-
-function recuperar() {
-    let recuperoLS = JSON.parse(localStorage.getItem('carrito'))
-    if(recuperoLS){
-      recuperoLS.forEach(item=>{
-        mostrarCarrito(item)
-        carrito.push(item)
-        totalCarrito()
-      })
-    }
-  }
-
-  function eliminar(){
-    
-  }
+  
 
 
 
 init();
+*/
